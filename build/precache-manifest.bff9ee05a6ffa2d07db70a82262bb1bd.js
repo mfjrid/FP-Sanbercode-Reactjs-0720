d7b290fa586eed8a2a4113f61d10df576d8129d2
MFJRID <mfjr48@gmail.com>
2020-08-15 20:36:03 +0700

    Mengubah title halaman

---
+++ build/precache-manifest.bff9ee05a6ffa2d07db70a82262bb1bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f809a9c20d4b2e9fa98a953f0e0d81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f0e756de1cc2130ab6d",
+    "url": "/static/css/main.fa7f1a50.chunk.css"
+  },
+  {
+    "revision": "80d19692d3b3848e4a69",
+    "url": "/static/js/2.f525a275.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f525a275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0e756de1cc2130ab6d",
+    "url": "/static/js/main.b4dc5c18.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "42f999413677f4d23d05d4a84fd5efeb",
+    "url": "/static/media/BackgroundLogin.42f99941.jpg"
+  }
+]);
